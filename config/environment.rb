4d05828d1e716c8c8f898d5fd1e631d746df9102
T-erry <terrywachuka10@gmail.com>
2023-02-21 18:25:09 +0300

    completed assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,21 @@
 require 'bundler'
 Bundler.require
-
+require 'pry'
 require_relative '../lib/dog'
 
-DB = { conn: SQLite3::Database.new("db/dogs.db") }
+DB = { conn: SQLite3::Database.new("db/dogs.db") } 
+
+# song1 = Dog.new(name:"Terry", breed:"Tabby")
+# song2 = Dog.new(name:"Frank", breed:"Scotish")
+def reset_database
+    Dog.drop_table
+    Dog.create_table
+    
+    
+    Dog.create(name: "Teddy",    breed: "cockapoo")
+    Dog.create(name: "Dave", breed: "poodle")
+  end
+  
+  reset_database
+
+# binding.pry
